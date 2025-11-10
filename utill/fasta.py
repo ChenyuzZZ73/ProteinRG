569936020933eea312918ea10daffa45715845bf
ChenyuzZZ73 <112918639+ChenyuzZZ73@users.noreply.github.com>
2025-11-11 00:42:18 +0800

    Add files via upload

---
+++ utill/fasta.py
@@ -0,0 +1,28 @@
+
+def save_as_fasta(data, path):
+    fasta = to_fasta(data)
+    with open(path,'w') as file:
+        file.write(fasta)
+
+def to_fasta(data):
+    fasta = ''
+    for seq,labels in data:
+        fasta += '>{}\n{}\n'.format(str(labels), seq)
+    return fasta
+
+import pandas as pd
+def fasta_file_to_df(path):
+    with open(path,'r') as file:
+        fasta = file.read()
+    df = []
+    name, seq = False, ''
+    for line in fasta.splitlines():
+        line = line.rstrip()
+        if line.startswith(">"):
+            if name:
+                df.append({'name':name, 'sequence':seq})
+            name = line[1:]
+            seq = ''
+        else: seq += line
+    df.append({'name':name, 'sequence':seq})
+    return pd.DataFrame(df)
